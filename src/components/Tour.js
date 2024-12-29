bac983dfb664e567e7ebf259d330a3dc153b55b3
Anurag Joshi <anuragj990@gmail.com>
2024-12-29 16:29:28 +0530

    2nd try

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -9,12 +9,12 @@ export const Tour = ({sptour, deleteTour}) => {
     <div style={{display: 'flex', flexDirection: 'column',margin: '20px', border: '1px solid black', padding: '10px', fontFamily: 'consolas', fontSize:'20px'}}>
         
         <h2>{sptour.name}</h2>
-        <p>{showmore? sptour.info : sptour.info.slice(0,199)}</p>
+        <p id={`tour-item-para-${sptour.id}`}>{showmore? sptour.info : sptour.info.slice(0,199)}</p>
         <img src={sptour.image} style={{height: '100px', width: '150px', border: '1px soild blue', borderRadius: '5px'}}/>
         <span>${sptour.price}</span>
 
          <button onClick={()=> setShow(!showmore)}>{showmore && 'Show less'}{!showmore && 'Show more'}</button>
-        <button onClick={()=> deleteTour(sptour.name)}>Delete this tour from list</button>
+        <button id={`delete-btn-${sptour.id}`} onClick={()=> deleteTour(sptour.name)}>Delete this tour from list</button>
     </div>
   )
 }
